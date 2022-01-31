f81528e21f57c7a1933577f92f45ea4d14a271bb
Antonio Alfonso <antonioalfonsomartinez255@gmail.com>
2022-01-31 22:30:27 +0100

    Creada ventana.

--- link_collector.py
+++ link_collector.py
@@ -4,3 +4,15 @@ from tkinter import *
 from tkinter import messagebox
 import json
 import webbrowser
+
+class Collector:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Link Collector")
+        self.root.geometry("530x590")
+
+        self.root.mainloop()
+
+if __name__=="__main__":
+    Collector()
+    
