3eba27cc34b77505e18f4c2f8db6c722662a2e16
Antonio Alfonso <antonioalfonsomartinez255@gmail.com>
2022-02-01 19:02:16 +0100

    Se redimensionó la ventana, se mportó 'os' y se añadió el entry del directorio actual.

--- link_collector.py
+++ link_collector.py
@@ -4,12 +4,18 @@ from tkinter import *
 from tkinter import messagebox
 import json
 import webbrowser
+import os
 
 class Collector:
     def __init__(self):
         self.root = Tk()
         self.root.title("Link Collector")
-        self.root.geometry("530x590")
+        self.root.geometry("575x650")
+
+        currentDir = StringVar()
+        currentDir.set(os.getcwd())
+
+        Entry(self.root,textvariable=currentDir,width=95).place(x=0,y=0)
 
         self.root.mainloop()
 
