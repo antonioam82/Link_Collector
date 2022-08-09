0d6abd7d79255df7a87e8e09a46eb6e17e6f93ce
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-08-09 09:43:15 +0200

    Update link_collector.py

--- link_collector.py
+++ link_collector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox, filedialog
 from urllib.parse import urlparse
 import threading
@@ -19,49 +19,49 @@ if not "my_link_list.json" in os.listdir():
 
 class Collector:
     def __init__(self):
-        self.root = Tk()
+        self.root = tk.Tk()
         self.root.title("Link Collector")
         self.root.geometry("575x623")
         self.root.configure(bg="gray88")
 
-        currentDir = StringVar()
+        currentDir = tk.StringVar()
         currentDir.set(os.getcwd())
-        self.my_url = StringVar()
+        self.my_url = tk.StringVar()
         self.selMode = 'normal'
-        self.search = StringVar()
+        self.search = tk.StringVar()
 
         with open("my_link_list.json") as f:
             self.link_list = json.load(f)
             #print(self.link_list)
 
-        Entry(self.root,textvariable=currentDir,width=95).place(x=0,y=0)
-        self.urlEntry = Entry(self.root,textvariable=self.my_url,width=43,font=("arial",18))
+        tk.Entry(self.root,textvariable=currentDir,width=95).place(x=0,y=0)
+        self.urlEntry = tk.Entry(self.root,textvariable=self.my_url,width=43,font=("arial",18))
         self.urlEntry.place(x=5,y=35)
-        Button(self.root,text="SAVE URL AS:",width=49,bg="gray77",command=self.enter_name).place(x=5,y=70)#79
-        Button(self.root,text="CLEAR URL",width=28,bg="gray77",command=self.clear_url).place(x=363,y=70)
-        self.canvas = Canvas(self.root,bg="black")
+        tk.Button(self.root,text="SAVE URL AS:",width=49,bg="gray77",command=self.enter_name).place(x=5,y=70)#79
+        tk.Button(self.root,text="CLEAR URL",width=28,bg="gray77",command=self.clear_url).place(x=363,y=70)
+        self.canvas = tk.Canvas(self.root,bg="black")
         self.canvas.place(x=5,y=110)
-        self.scrollbar = Scrollbar(self.canvas,orient=VERTICAL)
-        self.scrollbar.pack(side=RIGHT,fill=Y)
-        self.linkBox = Listbox(self.canvas,height=31,width=55)# 32 55
+        self.scrollbar = tk.Scrollbar(self.canvas,orient=tk.VERTICAL)
+        self.scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
+        self.linkBox = tk.Listbox(self.canvas,height=31,width=55)# 32 55
         self.linkBox.pack()
         self.linkBox.config(yscrollcommand = self.scrollbar.set)
         self.scrollbar.config(command = self.linkBox.yview)
-        self.searchEntry = Entry(self.root,textvariable=self.search,font=("arial",14),width=13)
+        self.searchEntry = tk.Entry(self.root,textvariable=self.search,font=("arial",14),width=13)
         self.searchEntry.place(x=363,y=110)
-        Button(self.root,text="SEARCH",bg="gray77",command=self.search_name).place(x=513,y=110)
-        self.showAll = Button(self.root,text="SHOW ALL LINKS",width=28,command=self.show_all)
+        tk.Button(self.root,text="SEARCH",bg="gray77",command=self.search_name).place(x=513,y=110)
+        self.showAll = tk.Button(self.root,text="SHOW ALL LINKS",width=28,command=self.show_all)
         self.showAll.place(x=363,y=138)
-        self.numLinks = Label(self.root,text='{} LINKS'.format(len(self.link_list)),bg='black',fg='green',width=25,font=("arial",10))
+        self.numLinks = tk.Label(self.root,text='{} LINKS'.format(len(self.link_list)),bg='black',fg='green',width=25,font=("arial",10))
         self.numLinks.place(x=363,y=195)#205
-        Button(self.root,text="IMPORT NEW LINK",bg="gray77",width=28,height=2,command=self.init_copy).place(x=363,y=230)#245
-        Button(self.root,text="ACCESS",bg="gray77",width=28,height=2,command=self.init_task).place(x=363,y=280)#295
-        Button(self.root,text="DELETE",bg="gray77",width=28,height=2,command=self.remove_link).place(x=363,y=350)#365
-        Button(self.root,text="DELETE ALL",bg="gray77",width=28,height=2,command=self.delete_listbox).place(x=363,y=400)#415
-        Button(self.root,text="CLEAR SELECTION",bg="gray77",width=28,height=2,command=self.clear_selection).place(x=363,y=450)#465
-        self.selMod = Button(self.root,text="SELECTION MODE: NORMAL",bg="gray77",width=28,height=2,command=self.selection_mode)
+        tk.Button(self.root,text="IMPORT NEW LINK",bg="gray77",width=28,height=2,command=self.init_copy).place(x=363,y=230)#245
+        tk.Button(self.root,text="ACCESS",bg="gray77",width=28,height=2,command=self.init_task).place(x=363,y=280)#295
+        tk.Button(self.root,text="DELETE",bg="gray77",width=28,height=2,command=self.remove_link).place(x=363,y=350)#365
+        tk.Button(self.root,text="DELETE ALL",bg="gray77",width=28,height=2,command=self.delete_listbox).place(x=363,y=400)#415
+        tk.Button(self.root,text="CLEAR SELECTION",bg="gray77",width=28,height=2,command=self.clear_selection).place(x=363,y=450)#465
+        self.selMod = tk.Button(self.root,text="SELECTION MODE: NORMAL",bg="gray77",width=28,height=2,command=self.selection_mode)
         self.selMod.place(x=363,y=500)#515
-        Button(self.root,text="SAVE LIST",bg="gray77",width=28,height=2,command=self.write_doc).place(x=363,y=570)#575
+        tk.Button(self.root,text="SAVE LIST",bg="gray77",width=28,height=2,command=self.write_doc).place(x=363,y=570)#575
         
         self.show_list()
 
@@ -112,7 +112,7 @@ class Collector:
             if self.urlEntry.get() not in self.link_list.values():
                 is_url = self.validate_url(self.urlEntry.get())
                 if is_url:
-                    self.window = Tk()
+                    self.window = tk.Tk()######
                     self.window.geometry("470x300")
                     self.window.title("Link Name")
                     Label(self.window,text="ENTER LINK NAME",width=67).place(x=0,y=45)
@@ -143,7 +143,7 @@ class Collector:
             self.my_list = []
             c = 1
             for i in self.link_list:
-                self.linkBox.insert(END,(str(c)+"- "+i))
+                self.linkBox.insert(tk.END,(str(c)+"- "+i))
                 self.my_list.append(self.link_list[i])
                 c+=1
             self.showAll.configure(state='disabled') 
@@ -151,7 +151,7 @@ class Collector:
     def set_name(self,entry_name):
         if entry_name != "":
             self.window.destroy()
-            self.linkBox.delete(0,END)
+            self.linkBox.delete(0,tk.END)
             self.link_list[entry_name] = self.urlEntry.get()
             self.update_json()
             self.show_list()
@@ -168,7 +168,7 @@ class Collector:
             if message == "yes":
                 for i in self.linkBox.curselection():
                     del self.link_list[self.get_key(self.my_list[i])]
-                self.linkBox.delete(0,END)
+                self.linkBox.delete(0,tk.END)
                 self.update_json()
                 if self.showAll["state"]=='disabled':########################
                     self.show_list()
